1a1a0681feaed5a9a84f4f1abb8e75593503580b
wang <wang.sai@itforce.co.jp>
2021-08-06 16:19:00 +0900

    nodeサーバー

--- serve/index.js
+++ serve/index.js
@@ -1,1 +1,24 @@
-const express = require
+const express = require('express')
+
+const app = express()
+
+app.get('/products', function(req,res){
+    res.json({'success':true})
+})
+
+const PORT = process.env.PORT || '3001'
+ 
+app.listen(PORT,function(){
+    console.log('I am running!')
+})
+
+
+
+
+// const express = require('express')
+// const app = express()
+// const port = 3000
+
+// app.get('/products', (req, res) => res.send('Hello World!'))
+
+// app.listen(port, () => console.log(`Example app listening on port ${port}!`))
